9bd1bb7ab5b18dfb2c65ea2d7806eae8a5630623
DMoya-max <dmoya1873@gmail.com>
2026-05-07 20:45:38 -0500

    Mejoras en productos

--- sigif/apps/productos/views.py
+++ sigif/apps/productos/views.py
@@ -4,23 +4,18 @@ from .form import ProductoForm
 
 # haidhasdhuyhasd
 def productos(request):
-
-    return render(request, "productos/productos.html")
+    productos = Producto.objects.all()
+    return render(request, 'productos/productos.html',  {'productos': productos})
 
 def crear_producto(request): 
     if request.method == "POST":
         form = ProductoForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('listar_productos')
+            return redirect('productos')
     else:
         form = ProductoForm()
     return render(request, 'productos/crear_productos.html', {'form': form})
-
-
-def listar_producto(request):
-    productos = Producto.objects.all()
-    return render(request, 'productos/listar_productos.html', {'productos': productos})
 
 def actualizar_producto(request, id):
     product = get_object_or_404(Producto, id=id)
@@ -29,7 +24,7 @@ def actualizar_producto(request, id):
         form = ProductoForm(request.POST, instance=product)
         if form.is_valid():
             form.save()
-            return redirect('listar_productos') 
+            return redirect('productos') 
     else:
         form = ProductoForm(instance=product)
 
@@ -39,6 +34,6 @@ def eliminar_producto(request, id):
     product = get_object_or_404(Producto, id = id)
     if request.method == "POST":
         product.delete()
-        return redirect('listar_productos')
+        return redirect('productos')
     else:
         return render(request, 'productos/eliminar_productos.html', {'producto': product})
