ae2de769e8ebb134cd1c6fb79e68e9e6027977bd
SirEdward97 <edwarpipita@gmail.com>
2026-05-22 13:13:38 -0500

    Cambios realizados

--- sigif/apps/facturacion/admin.py
+++ sigif/apps/facturacion/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Cliente, Producto, Factura
+
+admin.site.register(Cliente)
+admin.site.register(Producto)
+admin.site.register(Factura)
--- sigif/apps/facturacion/models.py
+++ sigif/apps/facturacion/models.py
@@ -1,3 +1,25 @@
-from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class Factura(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    fecha = models.DateField(auto_now_add=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
