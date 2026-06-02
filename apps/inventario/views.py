eff1ca5456acb102b170bffa991f7d1d0f1db6eb
juanfe1923 <felipeuribe778@gmail.com>
2026-06-02 13:55:00 -0500

    inventario actualizado

--- apps/inventario/views.py
+++ apps/inventario/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from apps.productos.models import Producto
 
 def inventario(request):
 
-    return render(request, "inventario/inventario.html")
+    producto = Producto.objects.all()
+
+    contexto = {
+        'producto': producto
+    }
+
+
+    return render(request, "inventario/inventario.html", contexto)
 
 def inv_historial(request):
 
